193235840b609772302b2d6d0dd5760d01e1935f
DXU7GWP <chihoward@johndeere.com>
2022-12-10 17:53:25 -0600

    day1completewoohoo

--- day1/dayOne.py
+++ day1/dayOne.py
@@ -1,1 +1,24 @@
-print("go taylor swift woohoo")
+with open("dayOneInputs.txt", "r") as input:
+    lines = input.readlines()
+
+
+
+
+calories = []
+counter = 0
+for line in lines:
+    line = line.strip()
+    if line == "":
+        calories.append(counter)
+        counter = 0
+    else:
+        counter = counter + int(line)
+calories.append(counter)
+print(max(calories))
+
+calories.sort()
+calories.reverse()
+print(calories[0] + calories[1] + calories[2])
+
+print(sum(calories[:-3]))
+#removes the last 3 elements
